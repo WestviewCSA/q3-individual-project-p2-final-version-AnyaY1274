2524c553cb3bdec418d91111ea66ff01ec9347b1
1965220 <1965220@01-18108.pusd.dom>
2026-02-24 11:42:48 -0800

    Map Reading 2/24
    Finished implementing helper to read queue (coordinate) map files and
    stored in 2d array. yay!!!!

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -8,11 +8,11 @@ public class Runner {
 	private static String[][] map;
 	private static int rows;
 	private static int cols;
-	private static int num;
+	private static int nums;
 	
 	
 	public static void main(String[] name) { //main method needs to be exact
-		readTextFile("hardMap2");
+		readQueueFile("hardMap1C");
 	}
 	
 	
@@ -23,22 +23,23 @@ public class Runner {
 			//reading file
 			Scanner scanner = new Scanner(file);
 			
-			rows = Integer.parseInt(scanner.next());
-			cols = Integer.parseInt(scanner.next());
-			num = Integer.parseInt(scanner.next());
+			//saving how many total rows, columns, and sections are in the map
+			rows = Integer.parseInt(scanner.next()); //total amount of rows
+			cols = Integer.parseInt(scanner.next()); //total amount of columns
+			nums = Integer.parseInt(scanner.next()); //total amount of sections
 			
-			map = new String[rows*num][cols];
+			map = new String[rows*nums][cols]; //creating 2d array with row/col/section numbers
 			
-		
+			//add map values to 2d array map
 			for(int r = 0; r < map.length; r++) {
-				String oneRow = scanner.next();
+				String oneRow = scanner.next(); //set entire line to one string
 				for(int c = 0; c < map[0].length; c++) {
-					map[r][c] = oneRow.substring(c, c+1);
+					map[r][c] = oneRow.substring(c, c+1); //save each individual character to map
 				}
 			}
 			
 			
-			System.out.println(Arrays.deepToString(map));
+			System.out.println(Arrays.deepToString(map)); //printing map
 			scanner.close();
 		} 
 		
@@ -55,18 +56,35 @@ public class Runner {
 		try { 
 			//reading file
 			Scanner scanner = new Scanner(file);
-			
+
+			//saving how many total rows, columns, and sections are in the map
 			rows = Integer.parseInt(scanner.next());
 			cols = Integer.parseInt(scanner.next());
-			num = Integer.parseInt(scanner.next());
+			nums = Integer.parseInt(scanner.next());
 			
-			map = new String[rows*num][cols];
+			map = new String[rows*nums][cols]; //creating 2d array with row/col/section numbers
 			
-		
+			while(scanner.hasNext()) {
+				String val = scanner.next(); //individual character
+				
+				//row, col, section number for each individual character
+				int row = Integer.parseInt(scanner.next());
+				int col = Integer.parseInt(scanner.next());
+				int num = Integer.parseInt(scanner.next());
+				
+				map[row + (rows*num)][col] = val; //row location is equal to the row read in file + total amount of rows preceding it
+				
+			}
+			//setting every remaining empty location in array equal to "."
+			for(int r = 0; r < map.length; r++) {
+				for(int c = 0; c < map[0].length; c++) {
+					if(map[r][c] == null) {
+						map[r][c] = ".";
+					}
+				}
+			}
 			
-			
-			
-			System.out.println(Arrays.deepToString(map));
+			System.out.println(Arrays.deepToString(map)); //printing map
 			scanner.close();
 		} 
 		
