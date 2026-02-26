e7ef97f5aab7fe45dfcb7549e5b2838ca5148356
1965220 <1965220@01-18108.pusd.dom>
2026-02-26 11:52:19 -0800

    Checking Errors 2/26
    Made exception classes !!
    Added exceptions to Text map reading method
    Currently in process of adding exceptions to Coordinate map reading
    method.
    Exploring how to do Queue Approach.

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -12,21 +12,42 @@ public class Runner {
 	
 	
 	public static void main(String[] name) { //main method needs to be exact
-		readCoFile("easyMap1C");
+		//readCoFile("easyMap1C");
+		
+		try{
+			readCoFile("easyMap1C");
+		} catch (IllegalMapCharacterException e) {
+			System.out.println(e.getMessage());
+		}  catch (IncompleteMapException e) {
+			System.out.println(e.getMessage());
+		} catch (IncorrectMapFormatException e) {
+			System.out.println(e.getMessage());
+		} catch (IllegalCommandLineInputsException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	
-	public static void readTextFile(String fileName) {
+	public static void readTextFile(String fileName) throws IllegalMapCharacterException, IncompleteMapException, IncorrectMapFormatException, IllegalCommandLineInputsException {
 		File file = new File(fileName);
 		
 		try { 
 			//reading file
 			Scanner scanner = new Scanner(file);
 			
+			
 			//saving how many total rows, columns, and sections are in the map
+			
+			//if first line does not have only 3 integers, error.
+			
 			rows = Integer.parseInt(scanner.next()); //total amount of rows
 			cols = Integer.parseInt(scanner.next()); //total amount of columns
 			nums = Integer.parseInt(scanner.next()); //total amount of sections
+			
+			if(rows<=0 || cols <= 0 || nums<=0){
+				throw new IncorrectMapFormatException("IncorrectMapFormatException - Incorrectly formatted maps");
+			}
+			
 			
 			map = new String[rows*nums][cols]; //creating 2d array with row/col/section numbers
 			
@@ -35,12 +56,14 @@ public class Runner {
 				String oneRow = scanner.next(); //set entire line to one string
 				//check for not enough characters in row + not enough rows
 				if(oneRow.length() != cols) {
-					System.out.println("IncompleteMapException"); //need to add throw/catch (how to do?????)
+					throw new IncompleteMapException("IncompleteMapException - Incomplete map files such as not enough characters for a given row or too few rows");
+					
 				}
 				for(int c = 0; c < map[0].length; c++) {
 					//check for illegal characters
 					if(!(oneRow.substring(c, c+1).equals("w")) && !(oneRow.substring(c, c+1).equals("@")) && !(oneRow.substring(c, c+1).equals(".")) && !(oneRow.substring(c, c+1).equals("$")) && !(oneRow.substring(c, c+1).equals("|"))) {
-						System.out.println("IllegalMapCharacterException"); //need to add throw/catch (how to do?????)
+						throw new IllegalMapCharacterException("IllegalMapCharacterException - Illegal characters on map");
+						
 					}
 					else {
 						map[r][c] = oneRow.substring(c, c+1); //save each individual character to map
@@ -62,7 +85,7 @@ public class Runner {
 	}
 	
 	
-	public static void readCoFile(String fileName) {
+	public static void readCoFile(String fileName) throws IllegalMapCharacterException, IncompleteMapException, IncorrectMapFormatException, IllegalCommandLineInputsException {
 		File file = new File(fileName);
 		
 		try { 
@@ -70,13 +93,17 @@ public class Runner {
 			Scanner scanner = new Scanner(file);
 
 			//saving how many total rows, columns, and sections are in the map
-			if(!scanner.hasNextInt()) {
-				System.out.println("IncorrectMapFormatException");
-			}
+			
+			
 			
 			rows = Integer.parseInt(scanner.next());
 			cols = Integer.parseInt(scanner.next());
 			nums = Integer.parseInt(scanner.next());
+			
+			//if first line does not have only 3 integers, throw exception.
+			if(rows <= 0 || cols <= 0 || nums <= 0){
+				throw new IncorrectMapFormatException("IncorrectMapFormatException - Incorrectly formatted maps");
+			}
 			
 			map = new String[rows*nums][cols]; //creating 2d array with row/col/section numbers
 			
